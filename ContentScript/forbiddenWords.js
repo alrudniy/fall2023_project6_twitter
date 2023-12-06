32fcd07720d14122451b375088c6cd9bce719c05
Rosieluna <143671674+Rosieluna@users.noreply.github.com>
2023-12-06 02:06:57 -0500

    Update forbiddenWords.js

--- ContentScript/forbiddenWords.js
+++ ContentScript/forbiddenWords.js
@@ -1,5 +1,21 @@
 const forbiddenWords = [
     "csam",
     "sex",
-    "Deven",
+    "cp",
+    "kids bathroom",
+    "child bathing",
+    "toddler bath",
+    "kids shower time",
+    "bikini kids modeling",
+    "toddler bikini",
+    "potty train",
+    "naked toddler",
+    "naked child",
+    "skinny baby booty",
+    "potty party",
+    "loves being nude",
+    "nj"
+    "peeing kid",
+    "nude child",
+    "diaper free
   ];
